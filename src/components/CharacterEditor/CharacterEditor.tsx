aa6a10c1b45f429e8447c90fe062fa5c1664bf37
lexavdov <aleksey.vdovin97@mail.ru>
2025-08-10 08:32:39 +0300

    Fix CharacterEditor component

--- src/components/CharacterEditor/CharacterEditor.tsx
+++ src/components/CharacterEditor/CharacterEditor.tsx
@@ -1,1 +1,15 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import { Component } from 'solid-js';
+import './CharacterEditor.css';
+
+const CharacterEditor: Component = () => {
+  return (
+    <div class="character-editor">
+      <div class="character-editor-content">
+        <h2>Character Editor</h2>
+        <p>Character customization interface</p>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterEditor;
